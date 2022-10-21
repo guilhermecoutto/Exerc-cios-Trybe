6b057393f70ac36860f4018ee64f71e610e5a78c
Guilherme <coutodev@hotmail.com>
2022-10-21 14:52:14 -0300

    Procure o índice do valor 'Portfólio' do array menu

--- fundamentos/secao-4-introducao-a-java-script/dia-4.2-array-e-loop-for/parafixararray.js
+++ fundamentos/secao-4-introducao-a-java-script/dia-4.2-array-e-loop-for/parafixararray.js
@@ -2,4 +2,8 @@ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices);
+
+let indexOfPortfolio = menu.index0f('Portfólio');
+
+console.log(indexOfPortfolio);
  
