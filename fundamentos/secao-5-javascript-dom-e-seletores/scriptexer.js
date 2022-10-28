30d8f9f995cd7b3978b4bb9a8363085a2d05accc
Guilherme <coutodev@hotmail.com>
2022-10-28 19:49:16 -0300

    exercicio 4

--- fundamentos/secao-5-javascript-dom-e-seletores/scriptexer.js
+++ fundamentos/secao-5-javascript-dom-e-seletores/scriptexer.js
@@ -1,4 +1,4 @@
 document.getElementsByTagName("p")[1].innerText = "Programador senior !!!";
 document.getElementsByClassName("main-content")[0].style.background = "rgb(76,164,109)";
 document.getElementsByClassName("center-content")[0].style.background = "white";
-
+document.getElementsByTagName("h1")[0].innerText = "Exercício - JavaScript"
